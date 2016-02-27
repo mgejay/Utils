b6fa9e7923405cf7872186e4915ad1f35899c9d4
Chaosiity (Git) <hentscheljr1@aol.com>
2016-02-26 19:58:01 -0500

    Updates

--- src/me/adaptified/utils/command/Command_information.java
+++ src/me/adaptified/utils/command/Command_information.java
@@ -16,7 +16,7 @@ public class Command_information extends BukkitCommand<Utils> {
         sender.sendMessage(ChatColor.GREEN + "This plugin was made by Adaptified and Prozza, made to set the core of a server!");
         sender.sendMessage(ChatColor.RED + "This plugin's version is " + plugin.getVersion() + "!");
         sender.sendMessage(ChatColor.AQUA + "This plugin is running on " + plugin.getConfig().getString("server-name") + "!");
-        sender.sendMessage(ChatColor.RED + "The server's current forum are " + plugin.getConfig().getString("website"));
+        sender.sendMessage(ChatColor.RED + "The server's current forums are " + plugin.getConfig().getString("website"));
         sender.sendMessage(ChatColor.BLUE + "The server's owner is " + plugin.getConfig().getString("owner") + "!");
         return true;
     }
--- src/me/adaptified/utils/command/Command_mute.java
+++ src/me/adaptified/utils/command/Command_mute.java
@@ -3,6 +3,8 @@ package me.adaptified.utils.command;
 import java.util.ArrayList;
 import me.adaptified.utils.Utils;
 import net.pravian.bukkitlib.command.BukkitCommand;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -17,9 +19,18 @@ public class Command_mute extends BukkitCommand<Utils> {
     public boolean run(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 
         Player player = getPlayer(args[0]);
+
+        String reason = null;
+        if (args.length >= 1) {
+            reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+        }
+
         if (!muted.contains(player.getName())) {
-            muted.add(player.getName());
+
             Bukkit.broadcastMessage(ChatColor.RED + sender.getName() + " has just muted " + player.getName());
+            player.sendMessage(ChatColor.AQUA + "You were muted by " + sender.getName() + ".");
+
+            muted.add(player.getName());
             return true;
         }
 
